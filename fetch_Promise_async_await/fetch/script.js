e98e3998811168a6ba576ed8fbbd917c349e327d
marielesf <mariele.fonttana@gmail.com>
2020-07-26 00:33:21 -0300

    usando async e await in functions

--- fetch_Promise_async_await/fetch/script.js
+++ fetch_Promise_async_await/fetch/script.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', function () {
+  doFetch();
+  doFecthAsync();
+  executeDivisionPromise();
+  executeDivisionPromiseAsyncAwait();
+});
+
+function doFetch() {
   fetch('https://api.github.com/users/marielesf').then((res) => {
     res
       .json()
@@ -8,15 +15,29 @@ window.addEventListener('load', function () {
       .catch((error) => {
         console.log('erro na requisicao');
       });
-    divisionPromise(12, 0)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((errorNewpromise) => {
-        console.log('Falha na divisao' + errorNewpromise);
-      });
   });
-});
+}
+
+async function doFecthAsync() {
+  const res = await fetch('https://api.github.com/users/marielesf');
+  const json = await res.json();
+  console.log('doFecthAsync', json);
+}
+
+function executeDivisionPromise() {
+  divisionPromise(12, 0)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((errorNewpromise) => {
+      console.log('Falha na divisao' + errorNewpromise);
+    });
+}
+
+async function executeDivisionPromiseAsyncAwait() {
+  const div = await divisionPromise(12, 2);
+  console.log('Divisao Async: ', div);
+}
 
 function showData(data) {
   const user = document.querySelector('#user');
